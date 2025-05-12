413c6946f63c03e0fe09198bce3570682068b6c0
Fagner Menezes <fagnertavaresdemenezes2024@gmail.com>
2025-05-12 10:34:36 -0300

    criacao entidades

---
+++ src/main/java/br/com/fagnerdev/api_restful_adopet/model/StatusAdocao.java
@@ -0,0 +1,9 @@
+package br.com.fagnerdev.api_restful_adopet.model;
+
+public enum StatusAdocao {
+
+    AGUARDANDO_AVALIACAO,
+    APROVADO,
+    REPROVADO;
+
+}
---
+++ src/main/java/br/com/fagnerdev/api_restful_adopet/model/TipoPet.java
@@ -0,0 +1,6 @@
+package br.com.fagnerdev.api_restful_adopet.model;
+
+public enum TipoPet {
+    GATO,
+    CACHORRO;
+}
