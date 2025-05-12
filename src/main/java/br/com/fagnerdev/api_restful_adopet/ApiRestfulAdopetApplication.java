1a055179272f879d294de3161e5c7dd24657d92f
Fagner Menezes <fagnertavaresdemenezes2024@gmail.com>
2025-05-12 09:55:52 -0300

    primeiro commit

---
+++ src/main/java/br/com/fagnerdev/api_restful_adopet/ApiRestfulAdopetApplication.java
@@ -0,0 +1,13 @@
+package br.com.fagnerdev.api_restful_adopet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulAdopetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulAdopetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fagnerdev/api_restful_adopet/ApiRestfulAdopetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fagnerdev.api_restful_adopet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulAdopetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
