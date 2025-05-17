4ad19ac8005d3c8ece80ebac669360628ce33de3
Fagner Menezes <fagnertavaresdemenezes2024@gmail.com>
2025-05-16 21:38:07 -0300

    refatorando records

--- src/main/java/br/com/fagnerdev/api_restful_adopet/dto/DadosDetalhesPet.java
+++ src/main/java/br/com/fagnerdev/api_restful_adopet/dto/DadosDetalhesPet.java
@@ -2,8 +2,28 @@ package br.com.fagnerdev.api_restful_adopet.dto;
 
 import br.com.fagnerdev.api_restful_adopet.model.Pet;
 import br.com.fagnerdev.api_restful_adopet.model.TipoPet;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record DadosDetalhesPet(Long id, TipoPet tipo, String nome, String raca, Integer idade) {
+public record DadosDetalhesPet(
+
+        @NotNull
+        Long id,
+
+        @NotNull
+        TipoPet tipo,
+
+        @NotBlank
+        String nome,
+
+        @NotBlank
+        String raca,
+
+        @Min(0)
+        Integer idade
+) {
+
 
     public DadosDetalhesPet(Pet pet) {
         this(pet.getId(), pet.getTipo(), pet.getNome(), pet.getRaca(), pet.getIdade());
